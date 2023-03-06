c647a0a4de368ec3ce3aebbab05265ef99502824
NXD <38133441+nodirhajiev@users.noreply.github.com>
2023-03-06 16:29:12 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.57.0/rclone-v1.57.0-linux-amd64.zip
-unzip -q rclone-v1.57.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.57.0-linux-amd64:$PATH
+wget -q https://downloads.rclone.org/v1.61.1/rclone-v1.61.0-linux-amd64.zip
+unzip -q rclone-v1.61.1-linux-amd64.zip
+export PATH=$PWD/rclone-v1.61.1-linux-amd64:$PATH
